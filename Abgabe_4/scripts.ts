5a320771905a964cdeba7674c4c3b482f414c568
Bui <lena.bui@hs-furtwangen.de>
2021-11-26 15:45:26 +0100

    TS DOM und Events

---
+++ Abgabe_4/scripts.ts
@@ -0,0 +1,43 @@
+namespace testNamespace {
+
+    const inputIntpret: HTMLInputElement = <HTMLInputElement>document.getElementById("input-interpret"); 
+    const inputPrice: HTMLInputElement = <HTMLInputElement>document.getElementById("input-price");
+    const inputDate: HTMLInputElement = <HTMLInputElement>document.getElementById("input-eventtime");
+    const display: HTMLElement = <HTMLElement>document.querySelector("#display");
+    const myButton: HTMLButtonElement = <HTMLButtonElement>document.querySelector("#mache-etwas"); 
+
+
+    myButton.addEventListener("click", mybuttonHandler); 
+
+
+    console.log(inputIntpret);
+    console.log(inputPrice);
+    console.log(inputDate);
+
+
+    function mybuttonHandler(): void {
+         
+        let interpretValue: string = inputIntpret.value; 
+        let priceValue: number = Number(inputPrice.value); 
+        let dateValue: Date = new Date();
+
+        let newElement: HTMLDivElement = document.createElement("div"); 
+        let deleteButton: HTMLButtonElement = document.createElement ("button");
+        deleteButton.textContent = "Delete Event";
+
+        newElement.textContent = interpretValue + ";" + priceValue + ";" + dateValue;   
+        display.appendChild(newElement); 
+
+        newElement.appendChild(deleteButton); 
+    
+        deleteButton.addEventListener("click", function () { 
+            deleteEvent(newElement);
+        });
+
+    }
+
+    function deleteEvent(parentElement: HTMLDivElement): void {
+        console.log("deleteEvent wurde aufgerufen!");
+        display.removeChild(parentElement);
+    }
+}
