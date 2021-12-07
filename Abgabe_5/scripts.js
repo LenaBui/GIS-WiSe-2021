06a76e4cfdc350722b6fab403f717cc911f05e56
Bui <lena.bui@hs-furtwangen.de>
2021-12-07 14:28:42 +0100

    TS JSON und localStorage verbessert

--- Abgabe_5/scripts.js
+++ Abgabe_5/scripts.js
@@ -1,55 +1,70 @@
 "use strict";
-var useLocalStorage;
-(function (useLocalStorage) {
+var testNamespace;
+(function (testNamespace) {
     const inputIntpret = document.getElementById("input-interpret");
     const inputPrice = document.getElementById("input-price");
     const inputDate = document.getElementById("input-eventtime");
     const display = document.querySelector("#display");
     const myButton = document.querySelector("#mache-etwas");
-    const loadButton = document.getElementById("load-button");
+    let events = [];
     myButton.addEventListener("click", mybuttonHandler);
-    loadButton.addEventListener("click", loadButtonHandler);
+    document.addEventListener('DOMContentLoaded', load);
     console.log(inputIntpret);
     console.log(inputPrice);
     console.log(inputDate);
-    function loadButtonHandler() {
-        console.log("Load Button clicked");
-        let valueFromLocalStorage = localStorage.getItem("interpret_input");
-        console.log("aktuelle Wert im Local Storage: " + valueFromLocalStorage);
-        display.textContent = valueFromLocalStorage;
+    function load() {
+        let val = localStorage.getItem('events');
+        if (!val) {
+            return;
+        }
+        let eventsArr = JSON.parse(val);
+        events = events.concat(eventsArr);
+        console.log(events);
+        renderList();
+    }
+    function renderList() {
+        display.innerHTML = '';
+        for (let event of events) {
+            let newElement = document.createElement("div");
+            let deleteButton = document.createElement("button");
+            deleteButton.textContent = "Delete Event";
+            newElement.textContent = event.interpret + ";" + event.price + ";" + new Date(event.date).toLocaleString();
+            display.appendChild(newElement);
+            newElement.appendChild(deleteButton);
+            deleteButton.addEventListener("click", function () {
+                deleteEvent(event.interpret, event.price, event.date);
+            });
+        }
     }
     function mybuttonHandler() {
         let interpretValue = inputIntpret.value;
         let priceValue = Number(inputPrice.value);
         let dateValue = new Date(inputDate.value);
-        let array = [10.1, 15.9, 20.1, 11.1, 12.2, 1.1, 12.99, 2.1, 3.1, 25.2];
-        let arrayIGotFromLocalStorage;
-        let arrayString = JSON.stringify(array);
-        let newElement = document.createElement("div");
-        console.log("Enter Button clicked");
-        console.log("aktuelle Input: " + inputIntpret.value);
-        localStorage.setItem("interpret_input", inputIntpret.value);
-        localStorage.setItem("price_input", arrayString);
-        let stringFromLocalStorage = localStorage.getItem("price_input");
-        arrayIGotFromLocalStorage = JSON.parse(stringFromLocalStorage);
-        console.log("Das Array mit dem Key 'price_input' aus dem LocalStorage:", arrayIGotFromLocalStorage);
-        let deleteButton = document.createElement("button");
-        deleteButton.textContent = "Delete Event";
-        newElement.textContent = interpretValue + ";" + priceValue + ";" + dateValue;
-        display.appendChild(newElement);
-        newElement.appendChild(deleteButton);
-        deleteButton.addEventListener("click", function () {
-            deleteEvent(newElement);
-        });
-        function deleteEvent(parentElement) {
-            console.log("deleteEvent wurde aufgerufen!");
-            display.removeChild(parentElement);
+        if (!interpretValue || !priceValue) {
+            return;
         }
-        let concertEvent = {
-            interpret: "Mark Knopfler",
-            price: 10.1
-        };
-        console.log(concertEvent.interpret + " " + concertEvent.price);
+        update(interpretValue, priceValue, dateValue);
     }
-})(useLocalStorage || (useLocalStorage = {}));
+    function update(interpret, price, date) {
+        events.push({ interpret: interpret, price: price, date: date.toISOString() });
+        localStorage.setItem('events', JSON.stringify(events));
+        renderList();
+    }
+    function deleteEvent(interpret, price, date) {
+        console.log("deleteEvent wurde aufgerufen!");
+        let found = null;
+        for (let key in events) {
+            let event = events[key];
+            if (event.interpret === interpret && event.price === price && event.date === date) {
+                found = Number(key);
+                break;
+            }
+        }
+        if (found !== null) {
+            events.splice(found, 1);
+            localStorage.setItem('events', JSON.stringify(events));
+        }
+        renderList();
+    }
+})(testNamespace || (testNamespace = {}));
 //# sourceMappingURL=scripts.js.map
--- Abgabe_5/scripts.ts
+++ Abgabe_5/scripts.ts
@@ -1,81 +1,89 @@
-namespace useLocalStorage {
+namespace testNamespace {
+
+    interface ConcertEvent {
+        interpret: string;
+        price: number;
+        date: string;
+    }
 
     const inputIntpret: HTMLInputElement = <HTMLInputElement>document.getElementById("input-interpret");
     const inputPrice: HTMLInputElement = <HTMLInputElement>document.getElementById("input-price");
     const inputDate: HTMLInputElement = <HTMLInputElement>document.getElementById("input-eventtime");
     const display: HTMLElement = <HTMLElement>document.querySelector("#display");
     const myButton: HTMLButtonElement = <HTMLButtonElement>document.querySelector("#mache-etwas");
-    const loadButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("load-button");
 
+    let events: ConcertEvent[] = [];
 
     myButton.addEventListener("click", mybuttonHandler);
-    loadButton.addEventListener("click", loadButtonHandler);
+    document.addEventListener('DOMContentLoaded', load);
 
 
     console.log(inputIntpret);
     console.log(inputPrice);
     console.log(inputDate);
 
-    function loadButtonHandler(): void {
-        console.log("Load Button clicked");
-        let valueFromLocalStorage: string = localStorage.getItem("interpret_input");
-        console.log("aktuelle Wert im Local Storage: " + valueFromLocalStorage);
-        display.textContent = valueFromLocalStorage;
+    function load() {
+        let val = localStorage.getItem('events');
+        if (!val) {
+            return;
+        }
+        let eventsArr = JSON.parse(val);
+        events = events.concat(eventsArr);
+        console.log(events);
+        renderList();
     }
 
-    function mybuttonHandler(): void {
+    function renderList() {
+        display.innerHTML = '';
+        for (let event of events) {
+            let newElement: HTMLDivElement = document.createElement("div");
+            let deleteButton: HTMLButtonElement = document.createElement ("button");
+            deleteButton.textContent = "Delete Event";
 
+            newElement.textContent = event.interpret + ";" + event.price + ";" + new Date(event.date).toLocaleString();
+            display.appendChild(newElement);
+
+            newElement.appendChild(deleteButton);
+
+            deleteButton.addEventListener("click", function () {
+                deleteEvent( event.interpret, event.price, event.date);
+            });
+        }
+    }
+
+
+    function mybuttonHandler(): void {
         let interpretValue: string = inputIntpret.value;
         let priceValue: number = Number(inputPrice.value);
         let dateValue: Date = new Date(inputDate.value);
 
-        let array: number[] = [10.1, 15.9, 20.1, 11.1, 12.2, 1.1, 12.99, 2.1, 3.1, 25.2];
-
-        let arrayIGotFromLocalStorage: number[];
-
-        let arrayString: string = JSON.stringify(array);
-
-        let newElement: HTMLDivElement = document.createElement("div");
-        console.log("Enter Button clicked");
-        console.log("aktuelle Input: " + inputIntpret.value);
-        localStorage.setItem("interpret_input", inputIntpret.value);
-
-        localStorage.setItem("price_input", arrayString);
-        let stringFromLocalStorage: string = localStorage.getItem("price_input");
-
-        arrayIGotFromLocalStorage = JSON.parse(stringFromLocalStorage);
-
-        console.log("Das Array mit dem Key 'price_input' aus dem LocalStorage:", arrayIGotFromLocalStorage);
-
-        let deleteButton: HTMLButtonElement = document.createElement("button");
-        deleteButton.textContent = "Delete Event";
-
-        newElement.textContent = interpretValue + ";" + priceValue + ";" + dateValue;
-        display.appendChild(newElement);
-
-        newElement.appendChild(deleteButton);
-
-        deleteButton.addEventListener("click", function () {
-            deleteEvent(newElement);
-
-        });
-
-        function deleteEvent(parentElement: HTMLDivElement): void {
-            console.log("deleteEvent wurde aufgerufen!");
-            display.removeChild(parentElement);
+        if (!interpretValue || !priceValue) {
+            return;
         }
 
-        interface ConcertEvent {
-            interpret: string;
-            price: number;
+        update(interpretValue, priceValue, dateValue);
+    }
+
+    function update (interpret: string, price: number, date: Date) {
+        events.push({interpret: interpret, price: price, date: date.toISOString()});
+        localStorage.setItem('events', JSON.stringify(events));
+        renderList();
+    }
+
+    function deleteEvent( interpret: string, price: number, date: string): void {
+        console.log("deleteEvent wurde aufgerufen!");
+        let found: number = null;
+        for (let key in events) {
+            let event = events[key];
+            if (event.interpret === interpret && event.price === price && event.date === date) {
+                found = Number(key);
+                break;
+            }
         }
-
-        let concertEvent: ConcertEvent = {
-            interpret: "Mark Knopfler",
-            price: 10.1
-        };
-
-        console.log(concertEvent.interpret + " " + concertEvent.price);
-
+        if (found !== null) {
+            events.splice(found, 1);
+            localStorage.setItem ('events', JSON.stringify(events));
+        }
+        renderList();
     }
 }
